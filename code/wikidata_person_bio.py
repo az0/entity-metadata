c8dc581579324940029aebdb5908f1a0005fc0ca
Andrew Ziem <ahz001@gmail.com>
2020-07-09 09:19:03 -0600

    Overhaul handling of command line arguments
    Stop hard-coding values.
    
    Also, sleep after success.

--- code/wikidata_person_bio.py
+++ code/wikidata_person_bio.py
@@ -24,12 +24,9 @@ import time
 
 url = 'https://query.wikidata.org/sparql'
 
-data_dir = "../data/wikidata/"
 
-timeout_seconds = 120
-
-
-def get_dob(dob):
+def get_dob(dob, data_dir, timeout_seconds, success_sleep, error_sleep):
+    """Download a CSV file from Wikidata for a single date of birth"""
     dob_str = dob.isoformat()
     csv_fn = os.path.join(data_dir, dob_str+".csv")
     print(f'Querying the date of birth: {dob}')
@@ -67,7 +64,7 @@ def get_dob(dob):
             url, params={'query': query}, headers=headers, timeout=timeout_seconds)
     except requests.exceptions.ReadTimeout:
         print(f' {dob}: network read timeout')
-        time.sleep(10)
+        time.sleep(error_sleep)
         return
 
     is_error = False
@@ -83,7 +80,7 @@ def get_dob(dob):
     if is_error:
         # "One client (user agent + IP) is allowed 60 seconds of processing time each 60 seconds"
         # https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#Query_limits
-        time.sleep(60)
+        time.sleep(error_sleep)
         return
 
     hdr_row = 'person,personLabel,family_nameLabel,given_nameLabel,sex_or_genderLabel,dob,country_of_citizenshipLabel,ethnic_groupLabel'
@@ -93,38 +90,58 @@ def get_dob(dob):
     with open(csv_fn, 'wb') as f:
         f.write(result_r.text.encode('utf-8'))
 
+    time.sleep(success_sleep)
+
 
 def go():
-    if not os.path.exists(data_dir):
-        print('Making data directory: %s' % data_dir)
-        os.mkdir(data_dir)
-    if not len(sys.argv) == 4:
-        print('Argument 1: beginning year')
-        print('Argument 2: ending year')
-        print('Argument 3: concurrent processes')
-        print('Loop starts on January 1 of beginning year and continues through December 31 of ending year.')
-        print(f'Example: python3 {sys.argv[0]} 1950 1960 5')
-        sys.exit(1)
-    year_start = int(sys.argv[1])
-    year_end = int(sys.argv[2])
-    process_count = int(sys.argv[3])
-    if process_count > 5:
-        print('Warning: Wikidata has a limit of 5. See https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#Query_limits')
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Download personal biographies from Wikidata as CSV files')
+    parser.add_argument(
+        'beginning_year', help='begin downloading on January 1 of this year', type=int)
+    parser.add_argument(
+        'ending_year', help='end downloading on December 31 of this year', type=int)
+    parser.add_argument(
+        '-d', '--data-dir', help='destination directory for CSV files', default='data')
+    parser.add_argument(
+        '-p', '--parallel', help='number of parallel processes', default=1, type=int)
+    parser.add_argument('-e', '--error-sleep',
+                        help='number of seconds to sleep after an error', default=60, type=float)
+    parser.add_argument('-s', '--success-sleep',
+                        help='number of seconds to sleep after a success', default=0.5, type=float)
+    parser.add_argument(
+        '-t', '--timeout', help='number of seconds to wait for a response', default=120, type=int)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.data_dir):
+        print(f'Making data directory: {args.data_dir}')
+        os.mkdir(args.data_dir)
+    if args.parallel > 5:
+        print('Warning: Wikidata has a limit of 5 parallel queries. See https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#Query_limits')
     dates = []
-    d = datetime.date(year_start, 1, 1)
-    while d <= datetime.date(year_end, 12, 31):
+    d = datetime.date(args.beginning_year, 1, 1)
+    while d <= datetime.date(args.ending_year, 12, 31):
         dates.append(d)
         d += datetime.timedelta(days=1)
-    if process_count > 1:
+    kwargs = {'data_dir': args.data_dir,
+              'timeout_seconds': args.timeout,
+              'success_sleep':  args.success_sleep,
+              'error_sleep':  args.error_sleep}
+
+    if args.parallel > 1:
         # process in parallel
         from multiprocessing import Pool
-        pool = Pool(processes=process_count)
-        for i in pool.imap_unordered(get_dob, dates):
+        from functools import partial
+        pool = Pool(processes=args.parallel)
+        func = partial(get_dob, **kwargs)
+        for i in pool.imap_unordered(func, dates):
             pass
     else:
         # process one at a time
         for date in dates:
-            get_dob(date)
+            get_dob(date, **kwargs)
+
 
 if __name__ == '__main__':
     go()
