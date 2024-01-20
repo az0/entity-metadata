6d4a2f3a1dacff3b7325397655e7477a244b37a5
Andrew Ziem <ahz001@gmail.com>
2024-01-20 10:27:43 -0700

    Add support for VR snapshot from 2005

--- code/etl_voter_north_carolina.py
+++ code/etl_voter_north_carolina.py
@@ -14,15 +14,44 @@ ETL North Carolina voter registration files
 2. Narrow down the columns
 3. Output all counties to a single CSV file
 
-Source data available here:
+
+# Source data available here
+
 https://www.ncsbe.gov/results-data/voter-registration-data#current-data
 https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+https://dl.ncsbe.gov/index.html?prefix=data/Snapshots/
+
+
+# Layout change
+
+Compared to latest (January 2024) the 2005 snapshot (VR_20051125.txt) has different names
+name_suffix_lbl:name_sufx_cd
+gender_code:sex_code
+middle_name:midl_name
+
+This program standardizes them to the new names.
+
+
+# Encoding
+
+Documentation for snapshot states UTF-16-LE, but it is not.
+
+https://s3.amazonaws.com/dl.ncsbe.gov/data/Snapshots/layout_VR_Snapshot.txt
+
 """
 
 
 import glob
 import sys
 import pandas as pd
+
+FILE_ENCODING = 'iso-8859-1'
+FILE_SEP = '\t'
+COL_NAME_MAP = {
+    'sex_code': 'gender_code',
+    'name_sufx_cd': 'name_suffix_lbl',
+    'midl_name': 'middle_name'
+}
 
 
 def groupby(df, col):
@@ -38,16 +67,40 @@ def go():
         sys.exit(1)
     in_fn = sys.argv[1]
     out_fn = sys.argv[2]
-    usecols = ['county_id','voter_reg_num','ncid','status_cd','last_name','first_name','middle_name','name_suffix_lbl','race_code','ethnic_code','gender_code']
+    want_cols = ['county_id', 'voter_reg_num', 'ncid', 'status_cd', 'name_prefx_cd', 'last_name', 'first_name',
+                 'middle_name', 'midl_name', 'name_suffix_lbl', 'name_sufx_cd', 'race_code', 'ethnic_code', 'gender_code', 'sex_code']
     print(f'Reading tab-delimited file: {in_fn}')
-    df = pd.read_csv(in_fn, sep='\t', usecols=usecols, encoding='iso-8859-1')
+    df_header = pd.read_csv(in_fn, sep=FILE_SEP,
+                            encoding=FILE_ENCODING, nrows=0)
+    has_cols = df_header.columns
+    use_cols = list(set(want_cols) & set(has_cols))
+    missing_cols = list(set(want_cols) - set(has_cols))
+    print(f'File has columns: {has_cols}')
+    print(f'Columns are missing: {missing_cols}')
+    print(f'Using columns: {use_cols}')
+
+    import csv
+    df = pd.read_csv(in_fn, sep=FILE_SEP, encoding=FILE_ENCODING,
+                     usecols=use_cols, on_bad_lines='warn', quoting=csv.QUOTE_NONE)
     print('Row count: {:,}'.format(df.shape[0]))
+
+    print('Trimming whitespace')
+    string_cols = df.select_dtypes(include=['object'])
+    string_cols = string_cols.apply(lambda x: x.str.strip())
+    df[string_cols.columns] = string_cols
+
+    print('Standardizing the names of columns')
+    for old_col, new_col in COL_NAME_MAP.items():
+        if old_col in has_cols:
+            print(f'Renaming {old_col} to {new_col}')
+            df.rename(columns={old_col: new_col}, inplace=True)
+
     groupby(df, 'status_cd')
     groupby(df, 'race_code')
     groupby(df, 'ethnic_code')
     groupby(df, 'gender_code')
     print(f'Writing to CSV file: {out_fn}')
-    df.to_csv(out_fn, index=False) # encoding defaults to utf-8
+    df.to_csv(out_fn, index=False)  # encoding defaults to utf-8
 
 
 go()
